1816cb364df6593d832438066c4edacc08099526
Madhuri <madhurishakamuri@gmail.com>
2025-06-17 13:17:24 +0530

    Bcryting Password

--- src/main/java/com/instagram/instagramclone/config/SecurityConfig.java
+++ src/main/java/com/instagram/instagramclone/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.instagram.instagramclone.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
@@ -11,8 +12,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -29,7 +28,8 @@ public class SecurityConfig
         return
         http
             .csrf(customizer ->customizer.disable())                                  //diableing csrf for every request
-            .authorizeHttpRequests(request ->request.anyRequest().authenticated())    // enable authentication for every request
+            .authorizeHttpRequests(auth -> auth
+    .requestMatchers(HttpMethod.POST, "/user","/posts").permitAll().anyRequest().authenticated())    // enable authentication for every request  auth -> auth.requestMatchers("/user").permitAll()
             //http.formLogin(Customizer.withDefaults());                             //  enabling the password and username enable and give response as form
             .httpBasic(Customizer.withDefaults())                                    //  instead of form we get the response in postman
             .sessionManagement(session ->
@@ -51,7 +51,7 @@ public class SecurityConfig
     @Bean
     public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService) {
     DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService); 
-    provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+    provider.setPasswordEncoder(new BCryptPasswordEncoder(12));  //validating the bcyprt password by original password hashcode
     return provider;
    }
 
